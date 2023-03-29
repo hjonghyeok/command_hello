9f9f45ab04496928779b8b1fb3808511e4526410
hjonghyeok <whdgur1999@gmail.com>
2023-03-29 10:04:10 +0900

    Confilict resolved Github

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 // For command line git tutorial
 // first : Local repository modification
-# for command line git tutorial
-# second : Github modification
+// second : Github modification
 
 print("hello world")
 
